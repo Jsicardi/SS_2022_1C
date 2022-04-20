d72f3a091d3d4c5329e06f957560e462c2988ab4
sespina99 <sespina@itba.edu.ar>
2022-04-20 15:57:56 -0300

    fp now writes the last line

--- TP3/fp.py
+++ TP3/fp.py
@@ -1,5 +1,8 @@
 ##nro_de_evento,tiempo,fpleft,fp right
 
+from cv2 import ROTATE_90_COUNTERCLOCKWISE
+
+
 static_file = open('static_rand.txt')
 static_lines = static_file.readlines()
 n = float(static_lines[0])
@@ -8,7 +11,7 @@ static_file.close()
 
 results_file = open('result.txt')
 results_lines = results_file.readlines()
-fp_csv = open("fp.csv", "w")
+fp_csv = open("fp_n_20_1.csv", "w")
 fp_csv.write("event_number,time,fp_left,fp_right\n")
 
 i = 0
@@ -17,7 +20,6 @@ count_right = 0
 for line in results_lines:
     args = line.split()
     if len(args) == 1:
-        print(args[0])
         fp_csv.write(str(i) + "," + str(time) + "," + str(1-count_right/n)+ "," + str(count_right/n) +"\n")
         count_right = 0
         i += 1
@@ -26,5 +28,7 @@ for line in results_lines:
         x = float(args[1])
         if x >= half_x:
             count_right += 1
+    if line == results_lines[-1]:
+        fp_csv.write(str(i) + "," + str(time) + "," + str(1-count_right/n)+ "," + str(count_right/n) +"\n")
 results_file.close()
 fp_csv.close()
