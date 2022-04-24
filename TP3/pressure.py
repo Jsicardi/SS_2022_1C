71956114a57503b3d35bdcad1bb70831b33e680d
JSicardi <jsicardi@itba.edu.ar>
2022-04-24 13:34:06 -0300

    Added collisions with middle walls in pressure

--- TP3/pressure.py
+++ TP3/pressure.py
@@ -1,6 +1,6 @@
 static_path = "static_rand.txt"
-v_file = "v_0.02"
-fp_path = "fp_v_0.02"
+v_file = "v_0.04"
+fp_path = "fp_v_0.04"
 epsilon = 0.001
 
 static = open(static_path)
@@ -9,12 +9,13 @@ lines_s = static.readlines()
 total_particles = int(lines_s[0].replace(" ",""))
 width = float(lines_s[1].replace(" ",""))*2
 heigth = float(lines_s[2].replace(" ",""))
+op_width = 0.02
 mass = float(lines_s[3].replace("   ","").replace("\n","").split(" ")[2])
-perimeter = width*2 + heigth*2
+perimeter = width*2 + heigth*2 + 2*(heigth-op_width)
 total_impulse = 0
 
 
-with open('p_0.02.csv', 'w') as f:
+with open('p_0.04_2.csv', 'w') as f:
     f.write("Pressure,Temperature\n")
 
     for i in range(1,11):
@@ -24,6 +25,7 @@ with open('p_0.02.csv', 'w') as f:
 
         #find eq_time
         j=0
+        total_impulse = 0
         for j in range(1,len(fp_lines)):
             
             tokens = fp_lines[j].split(',')
@@ -48,10 +50,10 @@ with open('p_0.02.csv', 'w') as f:
             if(len(tokens) == 1): #skip lines with timesteps
                 m+=1
                 continue
-            if (tokens[0] == "WC" and tokens[2] != "-5"): #timestep with wall colission detected and the wall is not the middle wall
+            if (tokens[0] == "WC"): #timestep with wall colission detected
                 particle_id = int(tokens[1])
                 particle_props = lines_r[m+particle_id+1].replace("\n","").split('\t')
-                if(tokens[2] == "-1" or tokens[2] == "-4"): #collision with left and right walls
+                if(tokens[2] == "-1" or tokens[2] == "-4" or tokens[2] == -5): #collision with left,right and middle walls
                     total_impulse += 2*mass*(abs(float(particle_props[3])))
                 else:
                     total_impulse += 2*mass*(abs(float(particle_props[4])))
@@ -60,10 +62,9 @@ with open('p_0.02.csv', 'w') as f:
         last_time = float(lines_r[len(lines_r) - total_particles - 3].replace("\n",""))
         force = total_impulse / (last_time-eq_time)
         pressure = force / perimeter
-        total_energy = 0
-        h=0
-        for h in range(len(lines_r) - total_particles, len(lines_r)): #temperature as average cinetic energy on eq
-            particle_props = lines_r[h].replace("\n","").split('\t')
-            total_energy += (1/2)*mass*(float(particle_props[3])**2 + float(particle_props[4])**2)
+        avg_energy = 0
+
+        particle_props = lines_r[3].replace("\n","").split('\t')
+        avg_energy = (1/2)*mass*(float(particle_props[3])**2 + float(particle_props[4])**2)
             
-        f.write("{0},{1}\n".format(pressure,total_energy/total_particles))
+        f.write("{0},{1}\n".format(pressure,avg_energy))
