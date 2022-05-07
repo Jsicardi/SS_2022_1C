a7d9097434e0e498db443f147feae5130360173e
Juan Quintairos <jquintairos@itba.edu.ar>
2022-05-07 17:05:02 -0300

    Added epsilon in check for print

--- TP4/src/main/java/OscilatorHelper.java
+++ TP4/src/main/java/OscilatorHelper.java
@@ -6,6 +6,8 @@ import java.math.RoundingMode;
 
 public class OscilatorHelper {
 
+    private final static double EPSILON = 0.00001;
+
     private final double k = 10000;
     private double m = 70;
     private final double A = 1;
@@ -46,7 +48,7 @@ public class OscilatorHelper {
         aPrev = getA(getForce(prevX, prevV));
 
         while(t <= finalT){
-            if(t % savingT == 0){
+            if(t % savingT < EPSILON || t % savingT > savingT - EPSILON){
                 generateOutput(p,t);
             }
             //calculate x
