5d1959edeeccfb9741f2d945259e5361592c7a76
JSicardi <jsicardi@itba.edu.ar>
2022-05-11 17:07:03 -0300

    Absorbed trajectories for probability distribution

--- TP4/absorbed_trajectory.py
+++ TP4/absorbed_trajectory.py
@@ -1,6 +1,6 @@
 import math,numpy as np
 path_format = "result_v"
-path_result = "avg_trajectory.csv"
+path_result = "absorbed_trajectory.csv"
 
 files = 9
 velocities = [5000,16250,27500,38750,50000]
@@ -8,7 +8,7 @@ velocities = [5000,16250,27500,38750,50000]
 def __main__():
     
     with open(path_result, "w") as f:
-        f.write("V0,Avg_trajectory,Std\n")
+        f.write("V0,Trajectory\n")
         for i in range(len(velocities)):
             trajectories = []    
             for j in range(1,files+1):
@@ -18,6 +18,9 @@ def __main__():
                 first_current = True
                 current_x = 0
                 current_y = 0
+                cut_type = int(lines[-1].replace("\n",""))
+                if(cut_type != 5):
+                    continue
                 for line in lines:
                     tokens  = line.replace("\n", "").split('\t')
                     if(len(tokens) < 4):
@@ -31,11 +34,8 @@ def __main__():
                         current_x = float(tokens[0])
                         current_y = float(tokens[1])
                 trajectories.append(distance)
-            sum_trajectory = 0
             for trajectory in trajectories:
-                sum_trajectory+=trajectory
-            stdev = np.std(trajectories)
-            f.write("{0},{1},{2}\n".format(velocities[i], sum_trajectory/files, stdev))
+                f.write("{0},{1}\n".format(velocities[i], trajectory))
 
                 
                 
--- TP4/proportion.py
+++ TP4/proportion.py
@@ -2,7 +2,7 @@ import math,numpy as np
 path_format = "result_v"
 path_result = "proportions.csv"
 
-files = 10
+files = 9
 velocities = [5000,16250,27500,38750,50000]
 
 def __main__():
@@ -11,7 +11,7 @@ def __main__():
         f.write("V0,Left,Right,Up,Down,Absorbed\n")
         for i in range(len(velocities)):
             cut_conditions = [0,0,0,0,0]
-            for j in range(1,files):
+            for j in range(1,files+1):
                 file = open("{0}{1}_{2}.txt".format(path_format,i+1,j))
                 lines = file.readlines()
                 cut_type = int(lines[-1].replace("\n",""))
