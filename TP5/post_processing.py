44726fd0dd98c0ae605c23a28d30da83f53685a3
sespina99 <sespina@itba.edu.ar>
2022-06-03 12:12:12 -0300

    ready csv of velocities and fz

--- TP5/post_processing.py
+++ TP5/post_processing.py
@@ -1,30 +1,50 @@
 import statistics
 
+import numpy
+
 
 path_format = "result_"
-path_answer = "fz.csv"
+path_answer_fz = "fz.csv"
+path_answer_velocity = "velocity.csv"
+
 runs = 10
 nhs = [2, 5, 10, 20, 40, 80, 160, 320]
 fzs = []
+nps = []
+velocities = []
 
-with open(path_answer, "w") as f:
-    f.write("nh,fz,stdev\n")
-    for nh in nhs:
-        np = 0
-        nh = 0
-        analized = False
-        for i in range(runs) and not analized:
-            file = open("{0}{1}_{2}.txt".format(path_format, nh, i))
-            lines = file.readlines()
-            for line in lines[::-1]:
-                tokens = line.replace("\n", "").split('\t')
-                if(len(tokens) == 1):
-                    analized = True
-                else:
-                    if tokens[4] == "0":
-                        nh += 1
-                    elif tokens[4] == "2":
-                        np += 1
-            fzs[i] = np/nh
-        f.write("{0},{1},{2}\n".format(nh,statistics.mean(fzs), statistics.stdev(fzs)))
+fz = open(path_answer_fz, "w")
+fz.write("nh,fz,stdev\n")
+fv = open(path_answer_velocity, "w")
+fv.write("nh,velocity,stdev\n")
+for nh in nhs:
+    np = 0
+    nh = 0
+    #itero para guardar la velocidad de contagio y el Fz del final de la simulacion
+    for i in range(runs):
+        iteration = -1
+        nps = 0
+        file = open("{0}_{1}_{2}.txt".format(path_format, nh, i))
+        lines = file.readlines()
+        for line in lines:
+            tokens = line.replace("\n", "").split('\t')
+            if(len(tokens) == 1):
+                #treat nps
+                if(iteration > 0):
+                    nps[iteration] = nps
+                nps = 0
+                nh = 0
+                iteration +=1
+            else:
+                if(tokens[4] == "2"):
+                    nps += 1
+                elif(tokens[4] == "0"):
+                    nh += 1
+        #add last iteration
+        nps[iteration] = nps
+        fzs[iteration] = nps/nh
+        #treat velocities
+        velocities[i] = statistics.mean(numpy.diff(nps))
+    fv.write("{0},{1},{2}\n".format(nh, statistics.mean(velocities), statistics.stdev(velocities)))
+    fz.write("{0},{1},{2}\n".format(nh,statistics.mean(fzs), statistics.stdev(fzs)))
             
